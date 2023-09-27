60732d9aa2c34ebd422fa8c2d6ebb2d4222788af
carly509 <tesnorcarly@gmail.com>
2023-09-27 13:24:49 -0400

    changes added

---
+++ src/components/EmailDetails.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="email-details">
+        <div class="email-nav">
+            <button @click="goBack" class="button-bck"><img src="../assets/icons/chevron_left_black_24dp.svg" alt="Newer" class="btn-icon-sm btn-icon-alt"> Back</button>
+        </div>
+        <h1 style="font-size:26px;margin-bottom:1rem;margin-left:1rem;">{{ message.subject }}</h1>
+        <div class="profile-container">
+            <div class="info-box">
+            <img src="https://s3.amazonaws.com/cdn.officepuzzle.com/avatars/143.png" class="avatar-img">
+            <p style="font-weight: bold;">{{ message.sender }}</p>
+            <p style="font-size:12px; margin: 3px;"> -{{ message.email }}-</p>
+          </div>
+        <div class="date-box">
+          <p>{{ message.date }}</p>
+          <button class="btn star" style="margin-top: -13px;">
+            <img src="../assets/icons/star_border_black_24dp.svg" alt="Not starred" class="btn-icon-sm btn-icon-alt btn-icon-hover message-btn-icon">
+          </button>
+        </div>
+        </div>
+      <p style="text-align: center;margin-top:12px;">{{ message.body }}</p>
+      <div class="btn-container">
+        <button class="btn action-btn" style="margin-top: -13px;"> Respond </button>
+        <button class="btn action-btn" style="margin-top: -13px;"> Transfer </button>
+      </div>
+    </div>
+  </template>
+
+  <script>
+  export default {
+    props: {
+      message: {
+        type: Object,
+        required: true
+      }
+    },
+    methods: {
+    goBack() {
+      this.$emit('go-back');
+    }
+  }
+  }
+  </script>
+
+  <style scoped>
+  .email-details {
+    margin-top: 20px;
+    padding: 1rem;
+  }
+  .profile-container {
+    display: flex;
+    justify-content: space-around;
+  }
+  .info-box {
+    display: flex;
+  }
+  .date-box {
+    display: flex;
+  }
+  .avatar-img {
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+  }
+  .btn-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 3rem;
+    border-top: 1px solid gray;
+  }
+  .action-btn {
+    border-radius: 30px;
+    width: 100px;
+    height: 30px;
+    border: 1px solid gray;
+    margin: 1rem;
+    margin-top: 1rem!important;
+  }
+  .email-nav {
+    background-color: #fff;
+    margin: 1rem;
+    margin-bottom: 3rem!important;
+  }
+  .button-bck {
+    border: none;
+    background-color: #fff;
+  }
+  </style>
--- src/components/Inbox.vue
+++ src/components/Inbox.vue
@@ -1,7 +1,68 @@
 <template>
-    <div>
+  <div class="mail">
+   <div class="inbox-message-item" :class="message.read ? 'inbox-message-item' : 'inbox-message-item message-default-unread'" @click="selectEmail">
+
+    <div class="checkbox" style="margin-right: -12px;" >
+      <button class="btn">
+        <img src="../assets/icons/check_box_outline_blank_black_24dp.svg" alt="Select" class="btn-icon-sm btn-icon-alt btn-icon-hover message-btn-icon">
+      </button>
+    </div>
+
+    <div class="message-group-hidden">
+      <button class="btn-alt btn-nofill drag-indicator" >
+        <img src="../assets/icons/drag_indicator_black_24dp.svg" alt="Drag" class="btn-icon-sm btn-icon-alt btn-icon-disabled" >
+      </button>
+    </div>
+
+    <button class="btn star" style="margin: 0;">
+      <img src="../assets/icons/star_border_black_24dp.svg" alt="Not starred" class="btn-icon-sm btn-icon-alt btn-icon-hover message-btn-icon">
+    </button>
+
+    <div class="message-default" >
+
+      <div :class="['message-sender', 'message-content', message.read ? '' : 'unread']" >
+        <span >{{ message.sender }}</span>
+      </div>
+
+      <div :class="['message-subject', 'message-content', message.read ? '' : 'unread']">
+        <span>{{ message.subject }}</span>
+      </div>
+
+      <div class="message-seperator message-content"> - </div>
+
+      <div class="message-body message-content">
+        <span> {{ message.body }}</span>
+      </div>
+
+      <div class="gap message-content" > &nbsp; </div>
+
+      <div :class="['message-date', 'center-text', message.read ? '' : 'unread']">
+        <span>{{ message.date }}</span>
+      </div>
 
     </div>
+
+    <div class="message-group-hidden" >
+      <div class="inbox-message-item-options">
+        <button class="btn">
+          <img src="../assets/icons/archive_black_24dp.svg" alt="Archive" class="btn-icon-sm btn-icon-alt btn-icon-hover">
+        </button>
+
+        <button class="btn">
+          <img src="../assets/icons/delete_black_24dp.svg" alt="Delete" class="btn-icon-sm btn-icon-alt btn-icon-hover">
+        </button>
+
+        <button class="btn">
+          <img src="../assets/icons/mark_as_unread_black_24dp.svg" alt="Mark as unread" class="btn-icon-sm btn-icon-alt btn-icon-hover">
+        </button>
+
+        <button class="btn">
+          <img src="../assets/icons/access_time_filled_black_24dp.svg" alt="Snooze" class="btn-icon-sm btn-icon-alt btn-icon-hover">
+        </button>
+      </div>
+    </div>
+   </div>
+  </div>
 </template>
 <script>
 export default {
@@ -9,6 +70,11 @@ export default {
     message: {
       type: Object,
       required: true
+    }
+  },
+  methods: {
+    selectEmail() {
+      this.$emit('select-email', this.message.id);
     }
   }
 }
